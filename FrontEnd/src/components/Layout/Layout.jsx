b7ab5d8648905c1d377ff84d3babec9df4a0b77d
Dip-to <2019-617-833@student.cse.du.ac.bd>
2024-03-29 02:03:19 +0600

    updated

--- FrontEnd/src/components/Layout/Layout.jsx
+++ FrontEnd/src/components/Layout/Layout.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import Header from '../Header/Header'
-import Footer from '../Footer/Footer'
-// import Routers from '../router/Routers'
 import Routers from '../../router/Routers'
 
 const Layout = () => {
